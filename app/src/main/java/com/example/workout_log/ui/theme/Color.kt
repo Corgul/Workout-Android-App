d921053e090bcf919849296310a4c61112f5cb78
Josh <jmurray@alarm.com>
2022-01-15 23:03:56 -0500

    initial commit

---
+++ app/src/main/java/com/example/workout_log/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.workout_log.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Grey800 = Color(0xFF121212)
+val Grey500 = Color(0xFF212121)
+val Grey200 = Color(0xFF484848)
+val Grey100 = Color(0xFFa2a2a2)
+val Black = Color(0xFF000000)
+
+val Red500 = Color(0xFFca3e47)
+val Red700 = Color(0xFF8e0000)
+val Red200 = Color(0xFFff5f52)
+
+val White = Color(0xFFffffff)
+val Green = Color(0xFF4CAF50)
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
